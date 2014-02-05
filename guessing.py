29f97672a5566bfa135997ecb09a85d880836b7b
Hackbright Student <user@mini-004.(none)>
2014-02-04 16:47:13 -0800

    asked for name, compared numbers, gave hints, counted no.of guess

--- guessing.py
+++ guessing.py
@@ -1,6 +1,34 @@
+
+numberToGuess = 61
+
 print "Howdy, what's your name?"
 
-name = raw_input ("(type in your name)")
+name = raw_input ("(type in your name )")
+
+# if
+# 
+# else 
+  #   print "This does not appear to be a name! Please try again using the alphabet."
+
 
 print "%s, I'm thinking of a number between 1 and 100. Try to guess my number." % name
 
+def TimeToGuess():
+    
+    num_of_guess = 0
+    while True:
+        guess = int(raw_input("Your guess? "))
+        num_of_guess = num_of_guess + 1
+    #if not guess.isdigit():
+     #   print "I'm sorry, that is not a valid number. Please try again."
+     #   guess = raw_input ("Your guess? ")
+    #elif guess != range(2,101):
+     #   "That number is outside of this program's range. Please choose a number between 1 and 100."
+        if guess > numberToGuess:
+            print "Your guess is too high, try again."
+        elif guess < numberToGuess:
+            print "Your guess is too low, try again."
+        else:
+            print "Well done, %s! You found my number in %s tries!" % (name, num_of_guess)
+            return False
+TimeToGuess()
