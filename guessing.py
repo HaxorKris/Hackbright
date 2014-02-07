c7e364b718ff04798180b9ac8f7e925fee0443ae
Hackbright Student <user@mini-004.(none)>
2014-02-07 12:14:58 -0800

    uploading corrected code

--- guessing.py
+++ guessing.py
@@ -2,49 +2,31 @@ import random
 numberToGuess = random.randrange(2,101)
 
 
-def GiveMeARealName():
+print "Howdy, what's your name?"
 
-    print "Howdy, what's your name?"
-    while True:
-        name = str(raw_input ("(type in your name) "))
-        if name.isalpha():
-            print "%s, I'm thinking of a number between 1 and 100. Try to guess my number." % name
-            return False
-        else:
-            print "This does not appear to be a name! Please try again using the alphabet."
-                
-        
+# Get user's name
+name = str(raw_input ("(type in your name) "))
 
-GiveMeARealName()
+print "%s, I'm thinking of a number between 1 and 100. Try to guess my number." % name
 
-def GiveAHint(guess, num_of_guess):
+# Initialize guess counter
+num_of_guess = 0
 
-    if guess > numberToGuess:
-        print "Your guess is too high, try again."
-    elif guess < numberToGuess:
-        print "Your guess is too low, try again."
+while num_of_guess > -1:
+    # Prompt for guess
+    guess = raw_input("Your guess? ")
+    # Increment our guess counter, regardless of validity of guess. 
+    num_of_guess = num_of_guess + 1
+    # Check for valid input.. 
+    if not guess.isdigit():
+        print "This is not a number! Please try a number between 1 and 100."
     else:
-        print "Well done! You found my number in %s tries!" % num_of_guess
+        guess = int(guess) 
+        if guess > numberToGuess:
+            print "Your guess is too high, try again."
+        elif guess < numberToGuess:
+            print "Your guess is too low, try again."
+        else:
+            print "Well done! You found my number in %s tries!" % num_of_guess
+            num_of_guess = -5
 
-def TimeToGuess():
-    
-    num_of_guess = 0
-    while True:
-        guess = raw_input("Your guess? ")
-        if guess.isdigit():
-            guess = int(guess)
-            GiveAHint(guess, num_of_guess)
-            
-        else: 
-            print "This is not a number! Please try a number between 1 and 100."
-            
-        num_of_guess = num_of_guess + 1
-    
-
-    #if not guess.isdigit():
-     #   print "I'm sorry, that is not a valid number. Please try again."
-     #   guess = raw_input ("Your guess? ")
-    #elif guess != range(2,101):
-     #   "That number is outside of this program's range. Please choose a number between 1 and 100."
-
-TimeToGuess()
