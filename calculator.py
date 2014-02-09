c0e8ff84237210152f65dff3bf3c459d10a888d0
Kris Weltz <kris@squirrel.local>
2014-02-09 15:37:24 -0800

    add arithmetic.py from Ex02, add user menu to calculator.py

---
+++ calculator.py
@@ -0,0 +1,18 @@
+user_input = []
+print " "
+print "         Ex03 Calculator"
+print " "
+print "The following functions are supported:"
+print "======================================"
+print "     addition (+ num1 num2)"
+print "     subtraction (- num1 num2)"
+print "     multiplication (* num1 num2)"
+print "     division (/ num1 num2)"
+print "     square root (square num1)"
+print "     cube root (cube num1)"
+print "     exponent (pow num1 num2)"
+print "     modulo (mod num1 num2)"
+print "========= type 'q' to quit =========="
+print "  "
+
+user_input = input("Calculator > ")
