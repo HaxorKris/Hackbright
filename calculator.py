bd415c4fa420cda0b402f642a8bff61b5489d731
Kris Weltz <kris@squirrel.local>
2014-02-09 16:22:37 -0800

    if/else'ing the menu options, print test string

--- calculator.py
+++ calculator.py
@@ -1,3 +1,4 @@
+calculation = ["temp"]
 
 print " "
 print "         Ex03 Calculator"
@@ -15,8 +16,33 @@ print "     modulo (mod num1 num2)"
 print "========= type 'q' to quit =========="
 print "  "
 
-user_input = raw_input("Calculator > ")   # prompt the user 
-calculation = user_input.split(" ")   # split the single string into 3 elements
-print calculation[0]
-print calculation[1] # checkpoint charlie -- does my string split work?
-print calculation[2]
+while calculation[0] != "q":
+	
+	user_input = raw_input("Calculator > ")   # prompt the user 
+	calculation = user_input.split(" ")   # split the single string into 3 elements
+	
+	print calculation[0]
+	print calculation[1] # checkpoint charlie -- does my string split work?
+	
+	print "Calculating . . ."
+	
+	if calculation[0] == "+":
+		print "You are adding"
+	elif calculation[0] == "-":
+		print "You are subtracting"
+	elif calculation[0] == "*":
+		print "You are multiplying"
+	elif calculation[0] == "/":
+		print "You are dividing"
+	elif calculation[0] == "square":
+		print "You are squaring"
+	elif calculation[0] == "cube":
+		print "You are cubing"
+	elif calculation[0] == "pow":
+		print "You are exponentiating"
+	elif calculation[0] == "mod":
+		print "You seek a remainder"
+	else:
+		print "This is not a valid option, please use the format: "
+		print "         'type num1 num2' e.g. '+ 1 2'"
+
