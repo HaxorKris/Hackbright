4898f3ca3346415a73d9d58e5e7de721f4025961
Hackbright Student <user@mini-004.(none)>
2014-02-10 18:10:19 -0800

    working on sort logic, does not work yet

--- wordcount.py
+++ wordcount.py
@@ -4,23 +4,41 @@ twain = open("twain.txt")
 text = twain.read() # gives a giant string
 
 lower_case = text.lower()
-#stripped_words = lower_case.strip(",")
 # "!\"#$%&'()*+,-./:;<=>?@[\\]^_`{}|~"
 words = lower_case.split()
 
-# words = split_words.lower()
-# print words
-
 dictionary = {}  # start an empty dictionary
 
-for k in words:
-    q = k.strip("!\"#$%&'()*+,-./:;<=>?@[\\]^_`{}|~")
+for i in words:
+    q = i.strip("!\"#$%&'()*+,-./:;<=>?@[\\]^_`{}|~")
     if dictionary.get(q):
         dictionary[q] += 1
     else:
         dictionary[q] = 1
-print dictionary
-    
 
+# for k, v in dictionary.iteritems():
+#     print k, v 
+
+#key = []
+
+for key in sorted(dictionary.iterkeys()):
+    print "%r: %r" % (key, dictionary[key])
+
+index_dictionary = {}
+
+for key, value in dictionary.iteritems():
+    index_dictionary[value] = [key]
+    
+for key, value in index_dictionary.iteritems():
+    print key, value
+
+    #key.append(k)
+
+#print key
+
+#sorted_key = key.sort()
+#print sorted_key
+
+#sorted_dict = sorted(dictionary, key=dictionary.get)
 
 twain.close()
