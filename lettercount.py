99af6c44567b566d561efc9ce940f87b28613db2
Kris Weltz <kris@squirrel.local>
2014-02-17 00:58:11 -0800

    1 am checkpoint

--- lettercount.py
+++ lettercount.py
@@ -1,13 +1,19 @@
 f = open("twain.txt")
-filecopy = f.read(1)
+filecopy = f.read()
 f.close()
 
 file_lowercase = filecopy.lower()
 
+running_total = [0] * 26    # sets a list of 26 0's, covered in class
+
 for char in file_lowercase:
-	if char == 'a':
-		tally += 1
-	print tally
+	if char.isalpha():
+#		if char == 'a':
+#			running_total[0] += 1
+#		elif char == 'b':
+#			running_total[1] += 1
+
+print running_total
 
 
 # maintain a tally for every letter
