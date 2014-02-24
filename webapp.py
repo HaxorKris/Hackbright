587a5364d8b5294c68df3772a9f1bd5974c37e1e
Student <user@chromebox-001.(none)>
2014-02-24 15:35:32 -0800

    added templates

--- webapp.py
+++ webapp.py
@@ -6,14 +6,22 @@ app = Flask(__name__)
 
 # Code goes here
 
-@app.route("/")
+@app.route("/student")
 # changed this from helloworld() to get_student
 # after we copied hackbright_app.py into the ENV directory
 def get_student():
 # let Flask connect to our database
     hackbright_app.connect_to_db()
-    student_github = request.args.get("student")
-    return hackbright_app.get_student_by_github(student_github)
+    student_github = request.args.get("github")
+    row = hackbright_app.get_student_by_github(student_github)
+    html = render_template("student_info.html", first_name=row[0], 
+                                last_name=row[1], github=row[2])
+    return html
+
+@app.route("/")
+def get_github():
+    return render_template("get_github.html")
+
 
 if __name__ == "__main__":
     app.run(debug=True)
