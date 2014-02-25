647138b1537c03d8b1b8fdce04b915f2f5d71d24
Student <user@chromebox-001.(none)>
2014-02-24 17:39:21 -0800

    more links backgit add hackbright_app.py

--- hackbright_app.py
+++ hackbright_app.py
@@ -52,10 +52,15 @@ def get_grade_by_student_project(student_github, project_title):
     Grade: %s """%(row[0], row[1], row[2], row[3])
 
 def get_grades_by_project(project_title):
-    query = """SELECT Students.first_name, Students.last_name, Grades.project_title, Grades.grade 
+#    query = """SELECT Students.first_name, Students.last_name, Grades.project_title, Grades.grade
+#        FROM Grades JOIN Projects ON (Grades.project_title = Projects.title)
+#        JOIN Students ON (Students.github = Grades.student_github)
+#        WHERE project_title = ?"""
+    query = """SELECT Students.first_name, Students.last_name, Grades.project_title, Grades.grade, Grades.student_github
         FROM Grades JOIN Projects ON (Grades.project_title = Projects.title)
         JOIN Students ON (Students.github = Grades.student_github)
         WHERE project_title = ?"""
+ 
     DB.execute(query, (project_title,))
     rows = DB.fetchall()
     return rows
